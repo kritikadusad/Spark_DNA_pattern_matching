74df1592657a1f51c835a0d6a42c06cc977da8d2
Kritika Dusad <kritikadusad@gmail.com>
2019-05-14 09:48:56 -0700

    removed commented functions.

--- network_exact_matching.py
+++ network_exact_matching.py
@@ -58,70 +58,24 @@ genomes = genomes.map(remove_first_line)
 
 # In[6]:
 
-
 genomes = genomes.map(lambda tup: (tup[0], tup[1].replace("\n", "")))
 # genomes.take(1)
 
 
 # In[7]:
 
-
-# # To read Fasta file.
-# genomes = sc.textFile("file:///home/krtika/*.fa")
-# # genome_n = genomes.map(lambda line: line if line[0]=='>' else "")
-# # genome_n.distinct().take(2)
-# # To save the identifier/name of the genome:
-# # genome_id = genomes.map(lambda genome: identifier(genome))
-# genome_name = genome_id.filter(lambda name: name[0]==">").take(1)[0]
-# # To remove identifier/name from the genome
-# genome_id = genomes.first()
-# header = sc.parallelize([genome_id])
-
-
-# In[ ]:
-
-
-
-
-
-# In[8]:
-
-
 pattern = 'GGGC'
 length = len(pattern)
 count = genomes.flatMap(lambda x: expand_line(x, length))     .map(lambda tup: position(tup, pattern))     .filter(lambda pos: pos if pos != None else "") 
-# count.take(10)
-# time_end = datetime.now()
-# print(time_end-time_start)
 
-
-# In[9]:
+time_end = datetime.now()
+print(time_end-time_start)
 
 
 df = sqlContext.createDataFrame(count, ['file name', 'position'])
 
-
-# In[ ]:
-
-
-
-
-
-# In[10]:
-
-
 grouped_df = df.groupby('file name').agg(collect_list('position').alias('position'))
 grouped_df.show()
 
-
-# In[11]:
-
-
 sc.stop()
-
-
-# In[ ]:
-
-
-
 
